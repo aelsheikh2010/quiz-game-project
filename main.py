ad8a1a5dc6a55341e192898a701af7522cce3938
Ahmed Elsheikh <golf17@gmail.com>
2023-02-02 02:19:01 +0200

    Initial commit

--- main.py
+++ main.py
@@ -13,3 +13,6 @@ for question in question_data:
 quiz = QuizBrain(question_bank)
 while quiz.still_has_question():
     quiz.next_question()
+
+print("you have completed the quiz")
+print(f"your final score was {quiz.score}/{quiz.question_number}")
